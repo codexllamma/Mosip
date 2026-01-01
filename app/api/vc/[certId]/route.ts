b69b5b971a35a5819ea557dbe4d82187447cb3d5
codexllamma <codexllamma@gmail.com>
2026-01-01 16:38:21 +0530

    wtv

--- app/api/vc/[certId]/route.ts
+++ app/api/vc/[certId]/route.ts
@@ -1,28 +1,25 @@
-import { NextResponse } from "next/server";
+import { NextResponse,NextRequest } from "next/server";
 import { prisma } from "@/lib/db";
 
 export async function GET(
-  req: Request,
+  request: NextRequest,
   { params }: { params: { certId: string } }
 ) {
   const { certId } = params;
-  console.log(certId);
+
   const certificate = await prisma.certificate.findUnique({
     where: { id: certId },
-    include: { vc: true }
+    include: { vc: true },
   });
 
   if (!certificate || !certificate.vc?.vcJson) {
     return new NextResponse("VC not found", { status: 404 });
   }
 
-  return NextResponse.json(
-    certificate.vc.vcJson,
-    {
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Content-Type": "application/json"
-      }
-    }
-  );
+  return NextResponse.json(certificate.vc.vcJson, {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": "application/json",
+    },
+  });
 }
